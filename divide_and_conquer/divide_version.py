f88fb0a674553bec74b337d0dd62ab1949d43996
Jian Geng <geng9650@gmail.com>
2024-04-22 17:10:58 +0800

    update

--- divide_and_conquer/divide_version.py
+++ divide_and_conquer/divide_version.py
@@ -5,6 +5,7 @@ def convex_hull_divide(points):
     sorted_points = sorted(points, key=lambda p: p[0])
     return convex_hull_recursive(sorted_points)
 
+
 def convex_hull_recursive(points):
     if len(points) <= 3:
         return points
@@ -13,8 +14,8 @@ def convex_hull_recursive(points):
     right_hull = convex_hull_recursive(points[mid:])
     return merge_hulls(left_hull, right_hull)
 
-def merge_hulls(left_hull, right_hull):
 
+def merge_hulls(left_hull, right_hull):
     lt = left_hull[-1]
     lb = left_hull[0]
     rt = right_hull[-1]
@@ -22,9 +23,9 @@ def merge_hulls(left_hull, right_hull):
     while True:
         olt, olb, ort, orb = lt, lb, rt, rb
         for rp in right_hull:
-            if orientation(lt, rt, rp) < 0:
+            if orientation(lt, rt, rp) > 0:
                 rt = rp
-            if orientation(lb, rb, rp) > 0:
+            if orientation(lb, rb, rp) < 0:
                 rb = rp
         for lp in left_hull:
             if orientation(rt, lt, lp) < 0:
@@ -33,6 +34,11 @@ def merge_hulls(left_hull, right_hull):
                 lb = lp
         if (olt, olb) == (lt, lb) and (ort, orb) == (rt, rb):
             break
-
-    
-    return left_hull
+    hull = []
+    for p in left_hull:
+        if orientation(lb, lt, p) >= 0:
+            hull.append(p)
+    for p in right_hull:
+        if orientation(rb, rt, p) <= 0:
+            hull.append(p)
+    return hull
